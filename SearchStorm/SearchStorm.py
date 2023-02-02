05d10f060071183fc261620eb0e98e4abb613703
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2023-02-02 01:45:22 +0000

    first commit

---
+++ SearchStorm/SearchStorm.py
@@ -0,0 +1,95 @@
+import os
+import argparse
+import re
+import concurrent.futures
+from termcolor import colored
+from columnar import columnar
+from click import style
+
+
+#from prettytable import PrettyTable
+# def print_result(result):
+#     table = PrettyTable(["\033[32mLine No.\033[0m", "\033[32mLine Content\033[0m", "\033[32mFile Name\033[0m", "\033[32mPattern\033[0m"])
+#     for match in result:
+#         table.add_row([match[0], match[1], match[2], match[3].pattern])
+#     print(table)
+
+def highlight_pattern(text, pattern):
+    start = text.find(pattern)
+    highlighted = text[start:start+len(pattern)]
+    new_text = text[:start] + style(highlighted, fg='blue') + text[start+len(pattern):]
+    #print(new_text)
+    return new_text
+
+def print_result(result, pattern):
+    headers = [ "Pattern ✨", " Line No. 🔧", "Line Content 🍑", "File Name 🎃"]
+    #print(result[-1])
+    #print(pattern)
+    patterns = [
+        ("Pattern", lambda text: style(text, fg="green")),
+        (" Line No.", lambda text: style(text, fg="cyan")),
+        ("Line Content", lambda text: style(text, fg="green")),
+        ("File Name", lambda text: style(text, fg="blue")),
+        (pattern, lambda text: style(text, fg="red")),
+        ("/[a-zA-z]+/", lambda text: style(text, fg="yellow")),
+        ("\d+", lambda text: style(text, fg="bright_green")),
+        (f"{pattern}.*", lambda text: style(text, fg="black")),
+    ]
+    
+    data = [[ match[3], match[0], highlight_pattern(match[1], pattern), match[2]] for match in result]
+    #print(data)
+    
+    table = columnar(data, headers,  patterns=patterns, justify="c", wrap_max=5, max_column_width=None)
+    print(table)
+
+
+def escape_regex_special_chars(string):
+    return re.escape(string)
+
+def search_file(file_path, pattern, original_pattern):
+    result = []
+    with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
+        content = f.readlines()
+        for i, line in enumerate(content):
+            if pattern.search(line):
+                result.append((i, line.strip(), file_path, original_pattern))
+    return result
+
+def search_files(directory, pattern, file_extension):
+    result = []
+    
+    # for output
+    original_pattern = pattern
+    pattern = escape_regex_special_chars(pattern) if '(' not in pattern else pattern
+    pattern = re.compile(pattern, re.IGNORECASE)
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_file = {executor.submit(search_file, os.path.join(root, file), pattern, original_pattern): os.path.join(root, file) for root, dir, files in os.walk(directory) for file in files if file.endswith(file_extension)}
+        for future in concurrent.futures.as_completed(future_to_file):
+            file = future_to_file[future]
+            try:
+                result.extend(future.result())
+            except Exception as e:
+                print(f"Error processing file {file}: {e}")
+    return result
+
+def _print(result, pattern):
+    if result:
+        # print("---------| -------------------------- | -------------------------- | -------------------------- ")
+        # for line_number, line, file, pattern in result:
+        #     print(f"Line: {line_number} | Line Content: {line} | File: {file} | Pattern: {pattern}")
+        print_result(result, pattern)
+    else:
+        print(colored("No match found", "red"))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Search for pattern in files recursively")
+    parser.add_argument("-d", "--directory", required=True, help="Directory to search")
+    parser.add_argument("-p", "--pattern", required=True, help="Pattern to search")
+    parser.add_argument("-e", "--extension", help="File extension to search (default: all files)")
+    args = parser.parse_args()
+
+    #pattern = escape_regex_special_chars(args.pattern)
+    result = search_files(args.directory, args.pattern, args.extension)
+    
+    _print(result, args.pattern)
---
+++ SearchStorm/__init__.py
@@ -0,0 +1,5 @@
+from .SearchStorm import search_files
+from .SearchStorm import print_result
+from .SearchStorm import _print
+
+__all__ = ["search_files", "print_result", "_print"]
